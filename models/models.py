4676a92a23f8be5bb2b8dcdc64f73f37f6ed007a
AnonymoUS2156 <nikgri@list.ru>
2024-02-14 11:40:46 +0300

    Созданы модели данных

---
+++ models/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Table, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+metadata = MetaData()
+
+synonyms = Table(
+    "synonyms",
+    metadata,
+    Column("first_string", String, nullable=False),
+    Column("second_string", String, nullable=True)
+)
+
